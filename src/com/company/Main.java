09876e359bd3071eeefc447bebf7522c40bdcadb
jtumms <john.tumminelli@tummsmedia.com>
2016-10-19 04:28:03 -0400

    Initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException{
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
+
+    }
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, username VARCHAR, address VARCHAR, email VARCHAR)");
+    }
+}
+
+
